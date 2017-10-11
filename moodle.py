33d6524ca7291e54ad1151b1e09fd67726ce14ee
rockrubio666 <monroyrubioc@gmail.com>
2017-10-11 14:38:46 -0500

    Con Funciones

--- moodle.py
+++ moodle.py
@@ -1,47 +1,59 @@
+
 import requests
 import socket
 import sys
 import getopt
 
-try:
-	opts, args = getopt.getopt(sys.argv[1:],'i:s:h', ['ip=', 'site=','help'])
-except getopt.GetoptError:
-	print 'opcion no valida'
-	print '-p, --ip Direccion IP del sitio'
-	print '-s, --site Direccion URL del sitio'
+ip = ''
+site = ''
+arg = ''
 
-	sys.exit(2)
+def help():
+	print('-p, --ip Direccion IP del sitio \n'
+		  '-s, --site Direccion URL del sitio\n'
+		  '-h, ---help Ayuda\n')
 
-for opt, arg in opts:
-	
-	if opt in ('-i','--ip'):
-		line = arg
-		try:
-			prueba = line.split('/')
-			socket.inet_aton(prueba[0])
-			site = socket.gethostbyaddr(prueba[0])
-			print "Direccion IP del sitio: " + prueba[0]
-			print "Direccion URL del sitio: " + site[0]
-		except socket.error:
-			print "ip no valida"
-	
-	elif opt in ('-s', '--site'):
-		line = arg
-		try:
-			prueba = line.split('/')
-			print "Direccion IP del sitio: " + socket.gethostbyname(prueba[0])
-			print "Direccion URL del sitio: " + line
-				
-		except:
-			print 'sitio no valido'
-	
-	elif opt in ('-h','--help'):
-		print '-p, --ip Direccion IP del sitio'
-		print '-s, --site Direccion URL del sitio'
-		print '-h, ---help Ayuda '
-		sys.exit(2)		
-			
-	else:
-		print 'opcion no valida'
-		sys.exit(2)
 
+def validateIP(ip):
+	try:
+		test = ip.split('/')
+		socket.inet_aton(test[0])
+		site = socket.gethostbyaddr(test[0])
+		print "Direccion IP del sitio: " + test[0]
+		print "Direccion URL del sitio: " + site[0]
+	except socket.error:
+		print "ip no valida"
+		
+def validateSite(site):
+	try:
+		test = site.split('/')
+		print "Direccion IP del sitio: " + socket.gethostbyname(test[0])
+		print "Direccion URL del sitio: " + site		
+	except:
+		print 'sitio no valido'
+	
+	
+def getParams(arg):
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],'i:s:h', ['ip=', 'site=','help'])
+	except getopt.GetoptError:
+		help()
+
+	for opt, arg in opts:	
+		if opt in ('-i','--ip'):
+			ip = arg
+			validateIP(ip)
+	
+		elif opt in ('-s', '--site'):
+			site = arg
+			validateSite(site)
+		
+		elif opt in ('-h','--help'):
+			h = arg
+					
+		else:
+			print 'opcion no valida'
+			sys.exit(2)
+
+
+getParams(arg)
