5c234eec83368c3f34f9a5f10e865d61cf35aa3e
rockrubio666 <monroyrubioc@gmail.com>
2017-10-11 19:27:56 -0500

    Moodle

--- moodle.py
+++ moodle.py
@@ -1,8 +1,9 @@
-
 import requests
 import socket
 import sys
 import getopt
+import urllib2
+import re
 
 ip = ''
 site = ''
@@ -13,22 +14,37 @@ def help():
 		  '-s, --site Direccion URL del sitio\n'
 		  '-h, ---help Ayuda\n')
 
+def ojsMoodleOther(arg):
+	response = urllib2.urlopen(arg)
+	page_source = response.read()
+	regex = re.compile(r'(.*)name="keywords" content="moodle(.*)')
+	match = regex.search(page_source)
+	if match.group():
+		print "Es un moodle"
+	else:
+		print "No es moodle"
+
+
 
 def validateIP(ip):
 	try:
 		test = ip.split('/')
+		#print test
 		socket.inet_aton(test[0])
 		site = socket.gethostbyaddr(test[0])
 		print "Direccion IP del sitio: " + test[0]
 		print "Direccion URL del sitio: " + site[0]
+		ojsMoodleOther(ip)
 	except socket.error:
 		print "ip no valida"
 		
 def validateSite(site):
 	try:
 		test = site.split('/')
-		print "Direccion IP del sitio: " + socket.gethostbyname(test[0])
+		#print test
+		print "Direccion IP del sitio: " + socket.gethostbyname(test[2])
 		print "Direccion URL del sitio: " + site		
+		ojsMoodleOther(site)
 	except:
 		print 'sitio no valido'
 	
@@ -57,3 +73,8 @@ def getParams(arg):
 
 
 getParams(arg)
+
+
+
+
+
