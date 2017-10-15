3eae4814f757bc18556f40fff2d385a115566f33
root <root@debian>
2017-10-15 06:11:19 -0500

    Crawler funcionando

--- crawler.py
+++ crawler.py
@@ -1,15 +1,30 @@
-import urllib2
+#!/usr/bin/python
+
 import re
+import requests
+from lxml import etree
+from lxml import html
 
-response = urllib2.urlopen('https://tuaulavirtual.educatic.unam.mx/')
-page_source = response.read()
-fo = open('ejemplo.text','w')
-fo.write(page_source)
-fo.close()
+arg = 'https://www.seguridad.unam.mx'
+#arg = 'https://aula.cert.unam.mx'
+#arg = 'https://tuaulavirtual.educatic.unam.mx/'
+res = requests.post(arg)
+page_source = res.text
+webpage = html.fromstring(res.content)
 
-fo = open('ejemplo.text','r')
-for line in fo:
-	matchObj = re.match(r'(.*) (src|href)=("(.*)")(.*)>', line)
-	if matchObj:
-		print matchObj.group() + "\n"
-fo.close()
+
+print "imagenes"
+for link in webpage.xpath('//img/@src'):
+	print link
+print "links"
+for link in webpage.xpath('//a/@href'):
+	print link
+print "java"
+for link in webpage.xpath('//script/@src'):
+	print link
+print "css"
+links = webpage.xpath('//head/link[@rel="stylesheet"]')
+hrefs = [l.attrib['href'] for l in links]
+print hrefs
+
+#####Falta: Organizarlo, recibir parametros, ingresar a cada sitio, separar los css
