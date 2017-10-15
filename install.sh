9195b2e8ae53bb47b934925fdc475f334f8b1b23
root <root@debian>
2017-10-15 05:58:22 -0500

    Update lxml

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 apt-get update
-apt-get install git python-pip python-argparse -y
-pip install requests bs4
+apt-get install git python-pip python-argparse libxml2-dev libxslt-dev python-dev -y
+pip install requests bs4 lxml
