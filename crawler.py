ab0f6a92928490326920812be64606c2029647a6
root <root@debian>
2017-10-15 17:55:16 -0500

    Crawler funcionando

--- crawler.py
+++ crawler.py
@@ -1,30 +1,60 @@
 #!/usr/bin/python
-
+# Existe un error que no pude solucionar
 import re
 import requests
+import sys
 from lxml import etree
 from lxml import html
 
-arg = 'https://www.seguridad.unam.mx'
-#arg = 'https://aula.cert.unam.mx'
-#arg = 'https://tuaulavirtual.educatic.unam.mx/'
-res = requests.post(arg)
-page_source = res.text
-webpage = html.fromstring(res.content)
+
+arg = sys.argv[1]
+
+def crawler(arg):
+	
+	if 'http://' in arg or 'https://' in arg: # Valida si tiene http(s)
+		# Lista para encontrar elementos
+		listFind = [ '//a/@href',  '//img/@src', '//script/@src', ('//head/link[@rel="stylesheet"]/@href')]
+		listLinks = []
+		
+		# Peticiones
+		res = requests.post(arg)
+		page_source = res.text
+		webpage = html.fromstring(res.content)
+
+	# Extrae los elementos de la pagina principal
+		i = 0
+		for i in range(0,len(listFind)):
+			for link in webpage.xpath(listFind[i]):
+				if link not in listLinks and '/' in link:
+					listLinks.append(link)
+			i + 1	
+		
+		# De los elementos encontrados trae sus recursos
+		element = 0
+		i = 0
+		for element in range(0,2):
+			res = requests.post(listLinks[element])
+			page_source = res.text
+			webpage = html.fromstring(res.content)
+			
+			for i in range(0,len(listFind)):
+				for link in webpage.xpath(listFind[i]):
+					if link not in listLinks and '/' in link:
+						listLinks.append(link)
+					
+				i + 1
+			element + 1
+		
+	
+	else: # Si no tiene http
+		http =  re.sub(r'(^)','http://',arg)
+		crawler(http)
+		exit(2)
+
+	print "Total de enlaces"
+	for l in range(len(listLinks)):
+			print listLinks[l]	
+	
+crawler(arg)
 
 
-print "imagenes"
-for link in webpage.xpath('//img/@src'):
-	print link
-print "links"
-for link in webpage.xpath('//a/@href'):
-	print link
-print "java"
-for link in webpage.xpath('//script/@src'):
-	print link
-print "css"
-links = webpage.xpath('//head/link[@rel="stylesheet"]')
-hrefs = [l.attrib['href'] for l in links]
-print hrefs
-
-#####Falta: Organizarlo, recibir parametros, ingresar a cada sitio, separar los css
