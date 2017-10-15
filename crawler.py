61d02edd42b7ad9d0062f9b3676340092005d0c7
root <root@debian>
2017-10-15 18:16:42 -0500

    Crawler manejo de errores

--- crawler.py
+++ crawler.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# Existe un error que no pude solucionar
 import re
 import requests
 import sys
@@ -17,11 +16,16 @@ def crawler(arg):
 		listLinks = []
 		
 		# Peticiones
-		res = requests.post(arg)
-		page_source = res.text
-		webpage = html.fromstring(res.content)
-
-	# Extrae los elementos de la pagina principal
+		try:
+			res = requests.post(arg)
+			page_source = res.text
+			webpage = html.fromstring(res.content)
+			
+		except:
+			print "Error, no se pudo consultar el sitio"
+			sys.exit(2)
+		
+		# Extrae los elementos de la pagina principal
 		i = 0
 		for i in range(0,len(listFind)):
 			for link in webpage.xpath(listFind[i]):
@@ -29,32 +33,42 @@ def crawler(arg):
 					listLinks.append(link)
 			i + 1	
 		
-		# De los elementos encontrados trae sus recursos
-		element = 0
-		i = 0
-		for element in range(0,2):
-			res = requests.post(listLinks[element])
-			page_source = res.text
-			webpage = html.fromstring(res.content)
-			
-			for i in range(0,len(listFind)):
-				for link in webpage.xpath(listFind[i]):
-					if link not in listLinks and '/' in link:
-						listLinks.append(link)
-					
-				i + 1
-			element + 1
+			# De los elementos encontrados trae sus recursos
+		try:
+			element = 0
+			i = 0
+			for element in range(0,2):
+				res = requests.post(listLinks[element])
+				page_source = res.text
+				webpage = html.fromstring(res.content)
+				
+				for i in range(0,len(listFind)):
+					for link in webpage.xpath(listFind[i]):
+						if link not in listLinks and '/' in link:
+							listLinks.append(link)
+				
+						i + 1
+					element + 1
+		except:
+			print "Total de enlaces:"
+			for l in range(len(listLinks)):
+				print listLinks[l]	
+			print "Error en algun enlace del subsitio"
+			sys.exit(2)
 		
-	
+		
+			
 	else: # Si no tiene http
-		http =  re.sub(r'(^)','http://',arg)
-		crawler(http)
-		exit(2)
+			http =  re.sub(r'(^)','http://',arg)
+			crawler(http)
+			exit(2)
 
-	print "Total de enlaces"
+	print "Total de enlaces:"
 	for l in range(len(listLinks)):
-			print listLinks[l]	
-	
+		print listLinks[l]	
+	sys.exit(2)
+					
+		
 crawler(arg)
 
 
