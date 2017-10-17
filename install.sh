e0ee20347f4f8a0636de1ccee0e940f43914e519
root <root@debian>
2017-10-16 19:17:20 -0500

    Tor Installation

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 apt-get update
-apt-get install git python-pip python-argparse libxml2-dev libxslt-dev python-dev -y
-pip install requests lxml
+apt-get install git python-pip python-argparse libxml2-dev libxslt-dev python-dev tor privoxy-y
+pip install requests lxml stem
+
+sed -i 's/^#\(.*\)ControlPort/ControlPort/g' /etc/tor/torrc
+sed -i 's/^#\(.*\)CookieAuthenticacion/CookieAuthenticacion/g' /etc/tor/torrc
+sed -i 's/^#\(.*\)forward-socks5\(.*\)127.0.0.1/	 forward-socks5   \/               127.0.0.1:9050 \./g' /etc/privoxy/config
+
+/etc/init.d/tor restart
+/etc/init.d/privoxy restart
+/etc/init.d/networking restart
