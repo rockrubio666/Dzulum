ef1f9c2f009c31f2a2d619a3749cb1145967f0eb
root <root@debian>
2017-10-30 02:13:56 -0500

    Updated

--- ojs.py
+++ ojs.py
@@ -6,6 +6,8 @@ import sys
 import requests
 from lxml import etree
 from lxml import html
+import wget
+import os
 
 
 arg = sys.argv[1]
@@ -15,46 +17,66 @@ log = ''
 def ojs(arg):
 	req = requests.post(arg)
 	page_source =  req.text
-	regex = re.compile(r'(.*)("((.*)\/index.php\/index\/login(.*))")(.*)')
-	match = regex.search(page_source)
-	try:
-		if match.group():
-			log = match.group(2).replace("\"","")
-			version(log)
-	except:
-		print 'nio'
-		sys.exit(2)
-	
-	
-		
-def version(log):
-	req = requests.post(log)
-	page_source =  req.text
-	regex = re.compile(r'(.*)(name="generator" content="(.*)")')
+	regex = re.compile(r'(.*)(name="generator" content="Open Journal System (.*)")')
 	match = regex.search(page_source)
 	try:
 		if match.group():
 			print "La version del sitio: " + arg + " es: " + match.group(3)
 	except:
-		print 'nio'
-		
+		files(arg)
 	
 
 
 def files(arg):
+	print 'entra hash'
 	m = hashlib.md5()
 	elements = []
-	listFind = [ '//script/@src', '//head/link[@rel="stylesheet"]/@href', '//img/@src']
+	listFind = [ '//script/@src', '//head/link[@rel="stylesheet"]/@href', '//img/@src','//link[@rel="shortcut icon"]/@href']
+	versions = {  
+		"2.3.5" : ['384772142d1907d7d3aea3ac11fad9d0',
+					'7d640303ec1bd0a376999f6e75f63c8d',
+					'65501be7c096bbe4646d5d3e9e345e62',
+					'f2e2f0c833643d76e4d923a86090ad35',
+					'4bdfef55f0a90d4b497a8a4207e3ff2b',
+					'66b6e69e47cee7e4d29a060b2a3c1da7',
+					'62efe9c068785969b1e8cb9ae9da83ed',
+					'1a3ddae3e99a081b70e05f8c33a6f96a',
+					'42e8ee2a3108a7891cde5f4c9791cdaf',
+					'f467f73c54fc5f27df5e54fd405f448a'],
 	
-	js = {  "2.3.7" : ['a728a0ecf3d220c30ce9b7dccc82d63e',
+		"2.3.6" : ['384772142d1907d7d3aea3ac11fad9d0',
+					'7d640303ec1bd0a376999f6e75f63c8d',
+					'65501be7c096bbe4646d5d3e9e345e62',
+					'f2e2f0c833643d76e4d923a86090ad35',
+					'4bdfef55f0a90d4b497a8a4207e3ff2b',
+					'66b6e69e47cee7e4d29a060b2a3c1da7',
+					'62efe9c068785969b1e8cb9ae9da83ed',
+					'1a3ddae3e99a081b70e05f8c33a6f96a',
+					'42e8ee2a3108a7891cde5f4c9791cdaf',
+					'f467f73c54fc5f27df5e54fd405f448a',
+					'3f262048db4232ff166095b7535c7872'],
+	
+	
+		"2.3.7" : ['a728a0ecf3d220c30ce9b7dccc82d63e',
 				   '4d5a879373b442c000f7d7814bb8abd7',
 				   '4eb3c3412061a1a8d69cb5de381fc87f',
-				   '0891d82f6de853a83328d2aa34322431'],
+				   '0891d82f6de853a83328d2aa34322431',
+				   'ae0daf35fd83499b0660a2c05bad2a09',
+				   '2951da4bfc58790a202e3f41e6dcdb0f',
+				   '6cc27529b60a1b27b49b558e401d509b',
+				   'ba87592c4b99cc8b31dead8461423a5a',
+				   '3de5ca485cf588415c99befd37e86f37',
+				   '8066ea1c35d4148fdb30c605d71f315c'],
 
 		"2.3.8" : ['a728a0ecf3d220c30ce9b7dccc82d63e',
 				'4d5a879373b442c000f7d7814bb8abd7',
 				'4eb3c3412061a1a8d69cb5de381fc87f',
-				'0891d82f6de853a83328d2aa34322431'],
+				'0891d82f6de853a83328d2aa34322431',
+				'ae0daf35fd83499b0660a2c05bad2a09',
+				'2951da4bfc58790a202e3f41e6dcdb0f',
+				'6cc27529b60a1b27b49b558e401d509b',
+				'ba87592c4b99cc8b31dead8461423a5a',
+				'3de5ca485cf588415c99befd37e86f37'],
 				
 		"2.4.0" : ['575bbf164dc5b03a5a4264d8714171c7',
 				 '327cdd668bfe85e308c04bff0fbd9206',
@@ -67,7 +89,14 @@ def files(arg):
 	 			 'a521f9aae90611966c189b1034864e70',
 				 '8bdc3f5637d9f6b8d4c78fd08eee9178',
 				 '4f89c4d276cc57a9760fd4ffd56de685',
-				 'f2d6c65d2328e57b9f752b21c08f25e0'],
+				 'f2d6c65d2328e57b9f752b21c08f25e0',
+				 'ce9ae3134e86687c9f435ee97ac1843b',
+				'b4d18405cc01a91fba05642f06f7d687',
+				'a253bfbec97d679d83f9b03b2d1eaa74',
+				'ea2b86521d24f5726289b5c2988eacaf',
+				'53782ed3741d65b84528923313a347c0',
+				'49e2e22c149cbdb61b9009eda4f7e8af',
+				'84bb8f3d3d8f6e4a2005f72966960936'],
 				 
 		"2.4.1" : ['575bbf164dc5b03a5a4264d8714171c7',
 				   '327cdd668bfe85e308c04bff0fbd9206',
@@ -80,7 +109,43 @@ def files(arg):
 				   'a521f9aae90611966c189b1034864e70',
 				   '8bdc3f5637d9f6b8d4c78fd08eee9178',
 				   '4f89c4d276cc57a9760fd4ffd56de685',
-				   'f2d6c65d2328e57b9f752b21c08f25e0'],
+				   'f2d6c65d2328e57b9f752b21c08f25e0',
+				   'ce9ae3134e86687c9f435ee97ac1843b',
+				'b4d18405cc01a91fba05642f06f7d687',
+				'a253bfbec97d679d83f9b03b2d1eaa74',
+				'ea2b86521d24f5726289b5c2988eacaf',
+				'53782ed3741d65b84528923313a347c0',
+				'49e2e22c149cbdb61b9009eda4f7e8af',
+				'3787a97f2aafca5f793806a1196fdd00'],
+				
+		"2.4.2" : ['575bbf164dc5b03a5a4264d8714171c7',
+				'327cdd668bfe85e308c04bff0fbd9206',
+				'e6f0011d8e092bbf8cb25174d3c8e921',
+				'4bb0100702a8e376906e821bbfef0d5c',
+				'87543c2f0a759c049e9bc604d33f71b2',
+				'bfa0f2db7fffe134d0036df368fc5c2a',
+				'63f7aaf40cd654ce09a966d31ddf0cbe',
+				'22579d0377978d7f1a32d9155c74f7a3',
+				'c20ce89f20bb36cb8454594e45d8a3eb',
+				'72d71affbc191a805148a08feed66105',
+				'74970e548eaf5354216dce5165c7e652',
+				'69efe8afc38ae44ee030d0c9a1aad90b',
+				'2c4ca2a6f23d53ab58576aeb58ce283d',
+				'a4e96c3ad33d9714fa2139d3dd475e88',
+				'3e193a54a00a5122890484e595f47f64',
+				'50183aa9790394c91a0ffb00cd8f61ad',
+				'5a98465baf74321256e20db000fc2654',
+				'a4409801f5d20f6437fc895246f62547',
+				'5e3533f747e8c110dbb46db898a98b93',
+				'1c9b997e1d842cdbedffeac3e02f51bc',
+				'1d9366fe647f81f49309abb2ba4311ee',
+				'fbd52ac399a24bc77eb0ff531a827593',
+				'24b772e55f13eaddc9f87ad501c0fbef',
+				'eb2e1f0af749d00a477e0c6cb5902b46',
+				'e8506f2c3a32ac2cfcf2b2c62d9e4edf',
+				'112aec7c26478ae58c7a4c1d804d3a41',
+				'2173265fd5089e9a2076c02cca5e458f',
+				'64cb7704a61ef9fc3718bbf4fd24a66e'],
 
 		"2.4.3" : ['575bbf164dc5b03a5a4264d8714171c7',
 				'327cdd668bfe85e308c04bff0fbd9206',
@@ -104,7 +169,14 @@ def files(arg):
 				'f21807a5a31b0217bf008305e0458f32',
 				'995e16162a346327bb4ba6f62223c5d0',
 				'1a7b7c9e3643f6a2db0b27e06b20fa35',
-				'86cd9b5925394f97f1d5e6d4a14c567d'],
+				'86cd9b5925394f97f1d5e6d4a14c567d',
+				'c1bf66c38ec2e50e55db0d82c4a388d8',
+				'd9a7ffc712e4e227853eb9833a689c47',
+				'ae0cd6570312cd106989a1cd7d58a51c',
+				'26a98cb2bb619c9a5f71b69f555b8ede',
+				'bf376d07e9a6762fa80e665a5481c392',
+				'bf255d62c5f374c9b6e87e0907cfcec4',
+				'e27c5b0b0607d5eeebf3c30ea63cac41'],
 		
 		"2.4.4" : ['575bbf164dc5b03a5a4264d8714171c7',
 				'327cdd668bfe85e308c04bff0fbd9206',
@@ -128,7 +200,15 @@ def files(arg):
 				'a7242d35b7aaec3289a2da6d1ae654a2',
 				'462503c62b63c40ecdb8af003d52a7af',
 				'387915ddef58996fe2d47f15a5352ce9',
-				'e576b2ea9b8e20c32571937cca1d07f2'],
+				'e576b2ea9b8e20c32571937cca1d07f2',
+				'9386a2da0d85b4a5e26a1637db3e54a7',
+				'0eee248118572387bf511675e1d32723',
+				'8436a250452061a9cba000f35a0c598d',
+				'e938e34f502933594633761797b6ba51',
+				'81edeb75afc1cc56737781579cb87064',
+				'b9955837b7145585064e8d98a9b9b415',
+				'92ef341f31e62d902e90cbed2214f625',
+				'36c11c67c4fa54e575835f309e9c7a97'],
 				
 		"2.4.5" : ['575bbf164dc5b03a5a4264d8714171c7',
 				 '327cdd668bfe85e308c04bff0fbd9206',
@@ -152,7 +232,15 @@ def files(arg):
 				 '6e9568a045688a2f123a7d5e24f1cd4e',
 				 'e49153d54250f07304a0439d6f9210f4',
 				 '313a42b70173033ccab787f39ec04314',
-				 'd8e4d5a3217d58f9bffeb643105a58ed'],
+				 'd8e4d5a3217d58f9bffeb643105a58ed',
+				 '9386a2da0d85b4a5e26a1637db3e54a7',
+				'0eee248118572387bf511675e1d32723',
+				'6005c209986f53288260702786727299',
+				'62cc7d93a4fac69ce3a9d0cfac8b063e',
+				'64208984de482391251d0ee51914fb1b',
+				'73f597562e3002214672fa1dd7bba930',
+				'b7bd502232c318098e6ce46381d82717',
+				'503026a5450579ac88198efd9f95282e'],
 	
 		"2.4.6" : ['575bbf164dc5b03a5a4264d8714171c7',
 				'327cdd668bfe85e308c04bff0fbd9206',
@@ -176,7 +264,15 @@ def files(arg):
 				'e2ff6047624a441b946048e94bd69e54',
 				'3dfeceb8fbe347ef1cb4ecc078553757',
 				'13adca952a8b74c21a01c7f0aa7489d1',
-				'9357302dbe74f2a1104cabe3b557c16d'],
+				'9357302dbe74f2a1104cabe3b557c16d',
+				'0231872d954707b6009758d77a4795f0',
+				'4c6847eff0f6345d3eab3582751af7c7',
+				'8164900a8b4226b9b6a21e319663685b',
+				'23b12b0e3a759c656c59db52393890d8',
+				'380182e45bf34e37e2be41a85e44c961',
+				'204920a1b3bc2628fc1123fcc134fd6e',
+				'949f18832d10d7fc742bd6370763e014',
+				'd19062c6da8ce2d1ea31d64e2506d1f9'],
 				
 		"2.4.7" : ['575bbf164dc5b03a5a4264d8714171c7',
 				 '327cdd668bfe85e308c04bff0fbd9206',
@@ -200,7 +296,15 @@ def files(arg):
 				'e2ff6047624a441b946048e94bd69e54',
 				'3dfeceb8fbe347ef1cb4ecc078553757',
 				'13adca952a8b74c21a01c7f0aa7489d1',
-				'9357302dbe74f2a1104cabe3b557c16d'],
+				'9357302dbe74f2a1104cabe3b557c16d',
+				'0231872d954707b6009758d77a4795f0',
+				'4c6847eff0f6345d3eab3582751af7c7',
+				'8164900a8b4226b9b6a21e319663685b',
+				'23b12b0e3a759c656c59db52393890d8',
+				'380182e45bf34e37e2be41a85e44c961',
+				'204920a1b3bc2628fc1123fcc134fd6e',
+				'949f18832d10d7fc742bd6370763e014',
+				'b37bf69c27af1c26802a75c88351e67f'],
 		
 	   "2.4.8" : [ '575bbf164dc5b03a5a4264d8714171c7', 
 				'327cdd668bfe85e308c04bff0fbd9206',
@@ -224,97 +328,42 @@ def files(arg):
 				'1072568e5e34f6248362968575ab03dd',
 				'094a8928109295742d46a67d4e1a4176',
 				'26a18c1f295a9868494a5a6b76b743fa',
-				'50e34fd34588f3a3a839b5d8482f76f5'],
-				
-		"3.0.2" : ['b0ea462d35bf9562497345655b1a1a0c',
-				   '898f59067992e0a8124fbbc2c073cf2c']}
-	
-	css = { "2.3.7" : ['ae0daf35fd83499b0660a2c05bad2a09',
-				'2951da4bfc58790a202e3f41e6dcdb0f',
-				'6cc27529b60a1b27b49b558e401d509b',
-				'ba87592c4b99cc8b31dead8461423a5a',
-				'3de5ca485cf588415c99befd37e86f37'],
-				
-		"2.3.8" : ['ae0daf35fd83499b0660a2c05bad2a09',
-				'2951da4bfc58790a202e3f41e6dcdb0f',
-				'6cc27529b60a1b27b49b558e401d509b',
-				'ba87592c4b99cc8b31dead8461423a5a',
-				'3de5ca485cf588415c99befd37e86f37'],
-				
-		"2.4.0" : ['ce9ae3134e86687c9f435ee97ac1843b',
-				'b4d18405cc01a91fba05642f06f7d687',
-				'a253bfbec97d679d83f9b03b2d1eaa74',
-				'ea2b86521d24f5726289b5c2988eacaf',
-				'53782ed3741d65b84528923313a347c0',
-				'49e2e22c149cbdb61b9009eda4f7e8af'],
-
-		"2.4.1" : ['ce9ae3134e86687c9f435ee97ac1843b',
-				'b4d18405cc01a91fba05642f06f7d687',
-				'a253bfbec97d679d83f9b03b2d1eaa74',
-				'ea2b86521d24f5726289b5c2988eacaf',
-				'53782ed3741d65b84528923313a347c0',
-				'49e2e22c149cbdb61b9009eda4f7e8af'],
-				
-		"2.4.2" : ['105fe52d0a376018b8eeba9ac4a286a9',
-				'2fd881bd435fdb5a86d5f8fd57fb5ba8',
-				'128566940998660da1c9673b88382add',
-				'83bceeaa6746f4ea125490d8b2376bf0',
-				'619bcd92ec1c27248f1955bdf559ca02',
-				'ecd29a9f3ede3b7d6f98db621f0900f8'],
-				
-		"2.4.3" : ['c1bf66c38ec2e50e55db0d82c4a388d8',
-				'd9a7ffc712e4e227853eb9833a689c47',
-				'ae0cd6570312cd106989a1cd7d58a51c',
-				'26a98cb2bb619c9a5f71b69f555b8ede',
-				'bf376d07e9a6762fa80e665a5481c392',
-				'bf255d62c5f374c9b6e87e0907cfcec4'],
-				
-		"2.4.4" : ['9386a2da0d85b4a5e26a1637db3e54a7',
-				'0eee248118572387bf511675e1d32723',
-				'8436a250452061a9cba000f35a0c598d',
-				'e938e34f502933594633761797b6ba51',
-				'81edeb75afc1cc56737781579cb87064',
-				'b9955837b7145585064e8d98a9b9b415',
-				'92ef341f31e62d902e90cbed2214f625'],
-				
-		"2.4.5" : ['9386a2da0d85b4a5e26a1637db3e54a7',
-				'0eee248118572387bf511675e1d32723',
-				'6005c209986f53288260702786727299',
-				'62cc7d93a4fac69ce3a9d0cfac8b063e',
-				'64208984de482391251d0ee51914fb1b',
-				'73f597562e3002214672fa1dd7bba930',
-				'b7bd502232c318098e6ce46381d82717'],
-				
-		"2.4.6" : ['0231872d954707b6009758d77a4795f0',
-				'4c6847eff0f6345d3eab3582751af7c7',
-				'8164900a8b4226b9b6a21e319663685b',
-				'23b12b0e3a759c656c59db52393890d8',
-				'380182e45bf34e37e2be41a85e44c961',
-				'204920a1b3bc2628fc1123fcc134fd6e',
-				'949f18832d10d7fc742bd6370763e014'],
-				
-		"2.4.7" : ['0231872d954707b6009758d77a4795f0',
-				'4c6847eff0f6345d3eab3582751af7c7',
-				'8164900a8b4226b9b6a21e319663685b',
-				'23b12b0e3a759c656c59db52393890d8',
-				'380182e45bf34e37e2be41a85e44c961',
-				'204920a1b3bc2628fc1123fcc134fd6e',
-				'949f18832d10d7fc742bd6370763e014'],
-				
-		"2.4.8" : ['08a481aaa1e7c3df7c12b9ccfc6f4cfb',
+				'50e34fd34588f3a3a839b5d8482f76f5',
+				'08a481aaa1e7c3df7c12b9ccfc6f4cfb',
 				'2c207fdf610ffde9c54167c4c2ed14d8',
 				'50b08c42387d02499736f9da90a87291',
 				'1a65bc4d2ad4e14634aaa857dc8d6c12',
 				'e31f8fe5668cf5cc44c2928aa5a85feb',
 				'706f2e680e3bc47e32fbba83086ec789',
-				'19454fa2ab19dc33aabc98bced3b1ce0'],
+				'19454fa2ab19dc33aabc98bced3b1ce0',
+				'2d500cf27b84bf8101ecc8c1d03f0f0b'],
+		
+		"3.0.0"	: ['b0ea462d35bf9562497345655b1a1a0c',
+					'3f2861b8ec7fb6c91cba210bc7a8d161',
+					'130da13d6103618eb7d45de190c30acb'
+					'950cf73a5f601dbc1aceea9367bb4de',
+					'125c862647362c7d205bd889889ce9c',
+					'd529d69d1df57044affe4f79a4eee24',
+					'9fa541c3add9fa089f1c4cf50f4c007c'],
+					
+		"3.0.1" : ['b0ea462d35bf9562497345655b1a1a0c',
+					'3f2861b8ec7fb6c91cba210bc7a8d161',
+					'8caf3662ce236d44a7edb1f900a1e04a'
+					'950cf73a5f601dbc1aceea9367bb4de',
+					'125c862647362c7d205bd889889ce9c',
+					'd529d69d1df57044affe4f79a4eee24',
+					'7c91b09ebdaac33137c763f06b709c2c'],
+					
+		"3.0.2" : ['b0ea462d35bf9562497345655b1a1a0c',
+				   '898f59067992e0a8124fbbc2c073cf2c',
+				   '6db1ce44ab7ab1d7263851bf7e94af37'
+				   '0c384bedcc1e813c9dc96665fd5b0c1',
+				   'bb87d41d15fe27b500a4bfcde01bb0e',
+				   '4e3706431052055bcd3aed2a06479b0',
+				   '293231fd3b3a687dc6dd6be0fdddca59']}
 				
-		"3.0.2" : ['6db1ce44ab7ab1d7263851bf7e94af37']}
+					
 	
-	img = { "3.0.2" : ['/templates/images/structure/logo.png',
-				'/templates/images/ojs_brand.png',
-				'/lib/pkp/templates/images/pkp_brand.png']}
-
 	
 	res = requests.post(arg)
 	page_source = res.text
@@ -330,21 +379,34 @@ def files(arg):
 	for i in range(0,len(listFind)):
 		for link in webpage.xpath(listFind[i]):
 			if domain in link:
+			#if 'localhost' in link:
 				req = requests.post(link)
-				if req.status_code == 200:
+				if req.status_code == 200 and i in range(2,3):
 					try:
+						print link 
+						filename = wget.download(link)
+						m.update(filename)
+						hs = m.hexdigest()
+						elements.append(hs)
+						os.remove(filename)
+					except:
+						continue
+				
+				else:
+					try:
+						print link
 						m.update(req.text)
 						hs =  m.hexdigest()
-						
 						elements.append(hs)
 					except:
 						continue
-						
-									
+				
 	for element in elements:
-		for key,value in js.iteritems():
+		for key,value in versions.iteritems():
 			if element in value:
 				print key
-		
+	
 
-files(arg)
+#files(arg)
+ojs(arg)
+
