57acce7fbc8df7bf1a62e765f7913742ac7eac22
root <root@debian>
2017-11-15 21:22:57 -0600

    Fixed

--- crawlerHead.py
+++ crawlerHead.py
@@ -17,50 +17,79 @@ def crawlerHead(url,f):
 	resources = []
 	sites = []
 	
-	
-	resources.append(url + '/ashfiemvmqwfhejfqkfwoe')
 		
+	if url.endswith('/'): # Si la URL termina con /, se agrega la cadena para validar multiples redirecciones
+		sites.append(url)
+		resources.append(url + 'ashfiemvmqwfhejfqkfwoe')
+		if os.path.exists(f):
+			fo = open(f, 'r')
+			for line in fo:
+				union = url + line
+				resources.append(union)	
+			fo.close()
+		else:
+			print "No se encontro el archivo"
 	
-	if os.path.exists(f):
-		fo = open(f, 'r')
-		for line in fo:
-			union = url + '/' + line
-			resources.append(union)	
-		fo.close()
 	else:
-		print "No se encontro el archivo"
+		sites.append(url + '/') # Si la URL no tiene /, se agrega la '/' y la cadena para validar multiples redirecciones
+		resources.append(url + '/ashfiemvmqwfhejfqkfwoe')
+		if os.path.exists(f):
+			fo = open(f, 'r')
+			for line in fo:
+				union = url + '/' + line
+				resources.append(union)	
+			fo.close()
+		else:
+			print "No se encontro el archivo"
 
-	sites.append(url)
+	requests.packages.urllib3.disable_warnings() # Se revisa el location que devuelve con el recurso que no exite
+	req = requests.head(resources[0], verify=False)
+	fake.append(req.url)
+	for key,value in req.headers.iteritems():
+		if 'location' in key:
+			f = value
 	
 	
-	for element in resources:
+	resources.pop(0)
+	print colored('Buscando recursos en: ', 'white') + colored(sites[0],'green')
+	
+
+	for element in resources: # Por cada elemento de la lista, se hace la peticion y se ve el codigo de estado
 		other  = element.rstrip()
 		requests.packages.urllib3.disable_warnings()					
 		res = requests.head(other, verify=False)
 		res.connection.close()
-		#res1 = requests.get(other)
 		
 		regex = re.compile(r'20[0-6]')
 		match = regex.search(str(res.status_code))
 		try:
-			if match.group():	
+			if match.group():	# Si el codigo de estado es 200, se muestra el recurso
 				if element not in sites:
 					sites.append(element)
 					print colored("Existe el recurso: ",'white') + colored(element, 'green')
 				else:
 					continue
 		except:
-			continue
-			regex = re.compile(r'30[0-7]')
+			regex = re.compile(r'30[0-7]') # Si el codigo de estado es 300
 			match = regex.search(str(res.status_code))
 			try:
 				if match.group():
-					print colored('Posible recurso: ') + colored(element,'green')
+					if len(fake) == 20:
+						print 'Se detuvo la ejecucion por los siguientes enlaces: '
+						for element in fake:
+							print colored(element, 'cyan')
+						break
+					else:
+						print colored('Posible recurso: ', 'yellow') + colored(element,'blue')				
+						for key, value in res.headers.iteritems():
+							if 'location' in key: 
+								if value in f:
+									fake.append(res.url)			
 			except:
 				continue
-			
-			
-			
+		
+	
+	
 def getParams(arg):
 	
 	parser = argparse.ArgumentParser(description='Crawler usando metodo HEAD',
