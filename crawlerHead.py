2a82503081242955e8ade277dbb80f7e1b9f1bc8
root <root@debian>
2017-11-22 00:59:01 -0600

    CrawlerHead in menu

--- crawlerHead.py
+++ crawlerHead.py
@@ -3,15 +3,15 @@
 import requests
 import re
 import sys
-import argparse
 import os
 from termcolor import colored
-arg = ''
+
+	
 f = ''
 url = ''
 
 def crawlerHead(url,f):
-	
+	print colored('\nBeginning Crawling with Head request', 'green')
 	fake = []
 	conLen = []
 	resources = []
@@ -90,30 +90,3 @@ def crawlerHead(url,f):
 		
 	
 	
-def getParams(arg):
-	
-	parser = argparse.ArgumentParser(description='Crawler usando metodo HEAD',
-		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	
-	
-	parser.add_argument('-u', '--url', help='Site URL', required=True)				
-	parser.add_argument('-f', '--file', help='File with resources', required=True)
-	
-	options = parser.parse_args()
-	
-	if len(sys.argv) == 1:
-		print parser.print_help()
-	
-	if options.url is not None and options.file is not None:
-		crawlerHead(options.url,options.file)
-	
-	
-		
-getParams(arg)
-
-
-
-
-
-
-
--- scaner.py
+++ scaner.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 from ojs import ojs
 from moodle import moodle
+from crawlerHead import crawlerHead
 
 arg = ''
 
@@ -14,9 +15,9 @@ def getParams(arg):
 	parser = argparse.ArgumentParser(description='Escaner de vulnerabilidades en OJS y Moodle',
 		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-	parser.add_argument('-o', '--ojs', help='URL from OJS',)
-	parser.add_argument('-m','--moodle', help='URL from Moodle')
-						
+	parser.add_argument('-o', '--ojs', metavar= 'URL', help = 'URL from OJS site')
+	parser.add_argument('-m','--moodle', metavar='URL', help = 'URL from Moodle site')
+	parser.add_argument('-C', '--crawlerHead', metavar='File', help = 'File with directories')	
 	options = parser.parse_args()
 	
 	if len(sys.argv) == 1 :
@@ -27,8 +28,14 @@ def getParams(arg):
 		
 	elif options.ojs in sys.argv:
 		ojs(options.ojs)
+		if options.crawlerHead in sys.argv:
+			crawlerHead(options.ojs,options.crawlerHead)
+	
 	elif options.moodle in sys.argv:
 		moodle(options.moodle)
+		if options.crawlerHead in sys.argv: 
+			crawlerHead(options.moodle,options.crawlerHead)
+	
 	
 	
 		
