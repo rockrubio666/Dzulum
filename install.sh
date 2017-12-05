57c1d5b65d18fe95b9d9926baf09502f95747418
root <root@debian>
2017-12-05 05:45:43 -0600

    Tor

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update
-apt-get install git python-pip python-argparse libxml2-dev libxslt-dev python-dev python-socksipy -y
+apt-get install git python-pip python-argparse libxml2-dev libxslt-dev python-dev tor python-socksipy -y
 pip install requests lxml wget termcolor gitpython 
 
 
