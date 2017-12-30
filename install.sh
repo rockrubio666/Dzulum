a9675cf480da1a8d79228820f0a7aebe3d7f61e9
root <root@debian>
2017-12-29 22:55:40 -0600

    Some problems solved

--- install.sh
+++ install.sh
@@ -3,9 +3,46 @@ apt-get update
 apt-get install git python-pip python-argparse libxml2-dev libxslt-dev python-dev tor python-socksipy privoxy -y
 pip install requests lxml wget termcolor gitpython 
 
-sed -i 's/^#\(.*\)ControlPort/ControlPort/g' /etc/tor/torrc
-sed -i 's/^#\(.*\)CookieAuthentication/CookieAuthentication/g' /etc/tor/torrc
-sed -i 's/^#\(.*\)forward-socks5\(.*\)127.0.0.1/	 forward-socks5   \/               127.0.0.1:9050 \./g' /etc/privoxy/config
+s='y'
+if /etc/init.d/privoxy restart; then
+	echo 'Everything is ok with privoxy'
+else
+	echo 'Could the installer remove all files of privoxy and try it again? [y/n]'
+	read privoxy
+	if [ "$privoxy" = "$s" ];then
+		apt-get remove prixovy -y
+		apt-get install prixovy -y
+		sed -i 's/^#\(.*\)forward-socks5\(.*\)127.0.0.1/	 forward-socks5   \/               127.0.0.1:9050 \./g' /etc/privoxy/config
+		if /etc/init.d/privoxy restart; then
+			echo 'Everything is ok with privoxy now!'
+		else
+			echo 'Something went wrong, please chek your configurations and try again.'
+		fi
+	else
+		echo 'Good luck! :,('
+	fi
+	
+fi
 
-/etc/init.d/tor restart
-/etc/init.d/privoxy restart
+if /etc/init.d/tor restart; then
+	echo 'Everything is ok with tor'
+	echo 'Enjoy the tool :D'
+else
+	echo 'Could the installer remove all files of tor and try it again? [y/n]'
+	read tor
+	if [ "$tor" = "$s" ];then
+		apt-get remove prixovy -y
+		apt-get install prixovy -y
+		sed -i 's/^#\(.*\)ControlPort/ControlPort/g' /etc/tor/torrc
+		sed -i 's/^#\(.*\)CookieAuthentication/CookieAuthentication/g' /etc/tor/torrc
+		if /etc/init.d/tor restart; then
+			echo 'Everything is ok with tor now!'
+			echo 'Enjoy the tool :D'
+		else
+			echo 'Something went wrong, please chek your configurations and try again.'
+		fi
+	else
+		echo 'Good luck! :,('
+	fi
+	
+fi
